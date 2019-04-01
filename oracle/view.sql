0747c1f31f221e5733fbd0812c3c854675450823
UsuarioTemporal <thomtwd@gmail.com>
2019-04-01 17:26:27 -0500

    Vistas

---
+++ oracle/view.sql
@@ -0,0 +1,17 @@
+-- vistas 
+
+/*las vistas es una consulta que se presenta como una tabla(virtual) a partir de un conjunto de tablas en una base de datos relacional.La vista solo almacena la definicion , no los datos.
+
+
+    Se usa para restringir el acceso a datos
+    Para crear consultas complejas
+    Proveer datos independientes
+    Presentar diferentes vistas de los mismos datos
+*/
+
+
+-- secuencias 
+
+--indices 
+
+-- sinonimos
