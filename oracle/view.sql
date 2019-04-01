090c9da9fd5f30f0043cdf788801b2a575cc15ac
UsuarioTemporal <thomtwd@gmail.com>
2019-04-01 17:42:51 -0500

    Estructura de una vista

--- oracle/view.sql
+++ oracle/view.sql
@@ -8,7 +8,8 @@
     Proveer datos independientes
     Presentar diferentes vistas de los mismos datos
 */
-
+create [or replace] [force | noforce] view view -- force : crea la vista asi las tablas que se este trabajando dentro de la vista no exista
+--noforce : solo crea la vista cuando las tabalas que esten relacionadas existan
 
 -- secuencias 
 
