3204adf434665193ee767be9891174682799f90e
UsuarioTemporal <thomtwd@gmail.com>
2019-04-01 17:51:04 -0500

    Simple view

--- oracle/view.sql
+++ oracle/view.sql
@@ -9,7 +9,14 @@
     Presentar diferentes vistas de los mismos datos
 */
 create [or replace] [force | noforce] view view -- force : crea la vista asi las tablas que se este trabajando dentro de la vista no exista
---noforce : solo crea la vista cuando las tabalas que esten relacionadas existan
+--noforce : solo crea la vista cuando las tabalas que esten relacionadas existan y este es por defecto
+as alias_view 
+
+create or replace view empleadoview as 
+select employee_id,last_name,salary from employees where departamento_id=80;
+
+desc empleadoview;
+select * from empleadoview;
 
 -- secuencias 
 
