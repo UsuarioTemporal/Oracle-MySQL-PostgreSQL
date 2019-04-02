f6a90c00caaffecfed03139acb59a35a602e93ad
UsuarioTemporal <thomtwd@gmail.com>
2019-04-02 15:12:07 -0500

    Comentario del dbms

--- oracle/plsql.sql
+++ oracle/plsql.sql
@@ -2,11 +2,19 @@
 declare
     x number := 10;
 begin
-    DBMS_OUTPUT.PUT_LINE(x);
+    DBMS_OUTPUT.PUT_LINE(x); -- PARA PODER VER ESTO SE NECESITA EL MODO DEPURACION ACTIVO
     begin
         DBMS_OUTPUT.PUT_LINE(x);
     end;
     begin
         DBMS_OUTPUT.PUT_LINE(x);
     end;
+end;
+
+begin
+    select username from dba_users;
+end;
+
+begin
+    null;
 end;
