c52d688bbaa49cc1d79165cc62c6b1ab9af1a4bd
UsuarioTemporal <thomtwd@gmail.com>
2019-04-03 14:51:40 -0500

    Cursores implicitos y explicitos

--- oracle/scripts4.sql
+++ oracle/scripts4.sql
@@ -12,3 +12,48 @@ declare
 begin
     SELECT nombre /* <-- este seria un cursor implicito */ into x from emepelados where codigo=1;
 end;
+
+/*
+Los cursores implícitos disponen de una serie de atributos
+    SQL%FOUND -- devuelve TRUE si el comando SQL ha devuelto al menos una fila
+    SQL%NOTFOUND --  devuelve TRUE si el comando SQL no ha devuetlo ninguna fila
+    SQL%ROWCOUNT  -- Numero de filas devueltas o procesadas por la operación
+*/
+
+DECLARE
+    nom ejemplo.nombre%type;
+BEGIN
+    SELECT nombre INTO nom FROM ejemplo WHERE codigo=1;
+    DBMS_OUTPUT.PUT_LINE(nom);
+    
+    IF SQL%FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('devolvio al menos una fila');
+    ELSIF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No devolvio ninguna fila');
+    else 
+        DBMS_OUTPUT.PUT_LINE('Soy por las puras :v');
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE(SQL%ROWCOUNT);
+    UPDATE ejemplo SET nombre='asdasd';
+    DBMS_OUTPUT.PUT_LINE(SQL%ROWCOUNT);
+END;
+
+/*
+Cursores explicitos
+Nos permiten trabajar con multiples registros al mismo tiempo
+*/
+declare 
+    cursor  nombre_cursor is select * from employees;
+    empleados employees%rowtype;
+begin
+open nombre_cursor;
+
+-- primera fila
+fetch nombre_cursor into empleados;
+DBMS_OUTPUT.PUT_LINE(empleados.first_name)
+--segunda fila
+fetch nombre_cursor into empleados;
+DBMS_OUTPUT.PUT_LINE(empleados.first_name)
+close nombre_cursor;
+end;
