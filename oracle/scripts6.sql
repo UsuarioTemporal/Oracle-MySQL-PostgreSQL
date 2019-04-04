905a77a92f4f53a8bac6982aee752c4d5bb682e1
UsuarioTemporal <thomtwd@gmail.com>
2019-04-04 11:00:06 -0500

    set SERVEROUT on

--- oracle/scripts6.sql
+++ oracle/scripts6.sql
@@ -1,6 +1,6 @@
 -- colecciones y tipos compuestos
 -- son componenetes que pueden albergar múltiples valores a diferencia de los escalares que solo pueden guardar un valor
-
+-- set SERVEROUT on ESTE COMANDO REDIRIGE LA SALIDA DEL BDMS HACIA LA SALIDA ESTANDAR
 /*
     Son de dos tipos :
 
