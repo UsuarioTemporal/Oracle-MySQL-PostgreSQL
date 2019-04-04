bd578f8496fcd5a637eafe98448f154526368dd0
UsuarioTemporal <thomtwd@gmail.com>
2019-04-04 11:23:28 -0500

    Records

--- oracle/scripts6.sql
+++ oracle/scripts6.sql
@@ -1,6 +1,6 @@
 -- colecciones y tipos compuestos
 -- son componenetes que pueden albergar múltiples valores a diferencia de los escalares que solo pueden guardar un valor
--- set SERVEROUT on ESTE COMANDO REDIRIGE LA SALIDA DEL BDMS HACIA LA SALIDA ESTANDAR
+-- set SERVEROUTput on ESTE COMANDO REDIRIGE LA SALIDA DEL BDMS HACIA LA SALIDA ESTANDAR
 /*
     Son de dos tipos :
 
@@ -20,6 +20,25 @@ TYPE empleado IS RECORD (
     nombre VARCHAR2(100),
     salario NUMBER,
     fecha employees.hire_date%TYPE,
-    datos completos employees%ROWTYPE
+    datos_completos employees%ROWTYPE
 );
 emple1 empleado;
+
+SET SERVEROUTput ON
+DECLARE
+    TYPE empelado IS RECORD (
+        nombre VARCHAR2(100),
+        salario NUMBER,
+        fecha employees.hire_date%TYPE,
+        datos_completos employees%ROWTYPE
+    );
+    emple1 empleado;
+BEGIN
+    SELECT * INTO emple1.datos_completos FROM
+    employees WHERE employee_id=100;
+    emple1.nombre:= emple1.datos_completos.first_name || ' '|| emple1.datos_completos.last_name;
+    emple1.salario:=emple1.datos_completos.salary*0.08;
+    emple1.fecha:=emple1.datos.hire_date;
+    --DBMS_OUTPUT.PUT_LINE(emple1); no se puede imprimir todo este objeto , tiene que ser algunos de sus valores
+    DBMS_OUTPUT.PUT_LINE(emple1.datos.first_name);
+END;
