378d768579c91174e510e85447ba356c937afd30
UsuarioTemporal <thomtwd@gmail.com>
2019-05-28 16:17:07 -0500

    Se termino el codigo del cliente

--- proyecto-db/scripts-db/ShoppingCar.sql
+++ proyecto-db/scripts-db/ShoppingCar.sql
@@ -3,4 +3,7 @@ create table client(
     name VaRCHAR2(50) NOT NULL,
     paternal_surname VARCHAR2(50) NOT NULL,
     maternal_surname VARCHAR2(50) NOT NULL,
+    dni VARCHAR2(8) unique not null check(length(dni)=8),
+    phone_number VARCHAR2(9) unique not null check(length(phone_number)=9),
+    email varchar2(100) unique not null
 );
