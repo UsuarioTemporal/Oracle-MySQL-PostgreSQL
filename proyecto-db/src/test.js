3bca16eb413efa9e72c0e02a2f8307a4f4196e6f
UsuarioTemporal <thomtwd@gmail.com>
2019-05-29 12:06:17 -0500

    Test coneccion  a oracle con async y await

--- proyecto-db/src/test.js
+++ proyecto-db/src/test.js
@@ -8,32 +8,9 @@ const oracle = require('oracledb'),
 // pool.then(e=>e.getConnection())
 const fnOracle = async ()=>{
     const pool = await oracle.createPool(database)
-    pool.getConnection((err,connection)=>{
-            if (err) return print(`Algo fallo ${err.code} ${err.message}`)
-            // connection.release()
-            connection.execute(SQL,(err,result)=>{
-                console.log(result.rows)
-                connection.close(err=>{
-                    if(err) return console.log(err.message)
-                });
-            })
-            return print('Conectado')
-        })
-
+    let connection = await pool.getConnection()
+    let result = await connection.execute(SQL)
+    return result.rows
 }
 fnOracle().then(print).catch(print)
-// oracle.getConnection(database,(err,connection)=>{
-//     if (err) return print(`Algo fallo ${err.code} ${err.message}`)
-//     // connection.release()
-//     connection.execute(SQL,(err,result)=>{
-//         console.log(result.rows)
-//         connection.close(err=>{
-//             if(err) return console.log(err.message)
-//         });
-//     })
-//     print('Conectado')
-//     return
-// })
-// oracle.query = promisify(oracle.query)
-// print(pool)
 module.exports = oracle
