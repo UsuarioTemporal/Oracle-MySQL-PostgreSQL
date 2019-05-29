14eabef9a5dd720d7a4c33a07255bfef588121cd
UsuarioTemporal <thomtwd@gmail.com>
2019-05-29 10:35:52 -0500

    Pruebaas en la BD

--- proyecto-db/src/database.js
+++ proyecto-db/src/database.js
@@ -1,7 +1,8 @@
 const oracle = require('oracledb'),
     {promisify}=require('util'),
     {database} = require('./keys'),
-    print = console.log
+    print = console.log,
+    SQL = `SELECT * FROM client`
 
 // lo que hace es tener una "ESPECIE" de "HILOS" que se iran ejecutando 
 // y cada uno irá haciendo una tarea a la vez en sequencia , esto nos ayudara en produccion
@@ -11,6 +12,12 @@ const oracle = require('oracledb'),
 oracle.getConnection(database,(err,connection)=>{
     if (err) return print(`Algo fallo ${err.code} ${err.message}`)
     // connection.release()
+    connection.execute(SQL,(err,result)=>{
+        console.log(result.rows)
+        connection.close(err=>{
+            if(err) return console.log(err.message)
+        });
+    })
     print('Conectado')
     return
 })
