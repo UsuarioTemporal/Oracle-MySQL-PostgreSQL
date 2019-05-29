fdae510130a2d1a2bd99aa7a01855206e1f39214
UsuarioTemporal <thomtwd@gmail.com>
2019-05-29 00:19:22 -0500

    Conexion a Oracle

--- proyecto-db/src/database.js
+++ proyecto-db/src/database.js
@@ -1,1 +1,7 @@
-const oracle = require('oracledb')
+const oracle = require('oracledb'),
+    {database} = require('./keys')
+
+// lo que hace es tener una "ESPECIE" de "HILOS" que se iran ejecutando 
+// y cada uno irá haciendo una tarea a la vez en sequencia , esto nos ayudara en produccion
+
+oracle.createPool(database);
---
+++ proyecto-db/src/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    database:{
+        user:'db_fact',
+        password:'db_fact%',
+        connectString:'localhost'
+    }
+}
