e2981818665738a1b7545f5bde73011cf998e6de
MaurickThom <thomtwd@gmail.com>
2019-06-13 01:15:35 -0500

    Controlando la insercion en una tabla

--- oracle/trigger.sql
+++ oracle/trigger.sql
@@ -15,9 +15,24 @@ de tipo database
 - shotdown
 */
 
-create or replace trigger tg_reg_AI
+create or replace trigger trg_reg_AI
 after insert on regions
 begin
     insert into log_table values('Insersion en la tabla de regiones',user);
+end;
+/
+
+-- impedir operaciones en los trigger
+-- para controlar que no se grabe en la fila
+
+-- solo el usuario hr pueda ingresar registros a la tabla
+create or replace trigger trg_reg_BI
+before insert on regions
+begin
+    --impedir que sys inserte filas
+    if user<>'hr' then
+        raise_application_error(-20000,'solo hr puede insertar en la tabla region');
+    end if;
+
 end;
 /
