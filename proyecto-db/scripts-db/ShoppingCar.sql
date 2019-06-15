f6da835cd35b69ea9c81d085684d8971ff14dd38
MaurickThom <thomtwd@gmail.com>
2019-06-15 11:35:53 -0500

    Procedimiento de to_buy

--- proyecto-db/scripts-db/ShoppingCar.sql
+++ proyecto-db/scripts-db/ShoppingCar.sql
@@ -391,15 +391,19 @@ select * from detail;
 
 create or replace procedure to_buy(product_ in number,quantity_ in number,price in number,user_ in number,bill_id number)
 is
-	bill_id_result number;
 	cursor cur_val_prod is select quantity from product where product_id=product_;
-	exception no_data;
+	no_data exception ;
+	cantidad number;
 begin
 	--
-	if cur_val_prod <quantity_ then
+	
+	for data_ in cur_val_prod loop
+		cantidad:=data_.quantity;
+	end loop;
+	if  cantidad < quantity_ then
 		raise no_data;
 	else 
-		insert into detail values (sq_detail.nextval,bill_id,product,quantity_,price,user_);
+		insert into detail values (sq_detail.nextval,bill_id,product_,quantity_,price,user_);
 		update product set quantity=quantity-quantity_ where product_id=product_;
 		
 		return;
