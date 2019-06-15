7881915bcbdcbdba0df28f5cc0940b61ebf1a7e4
MaurickThom <thomtwd@gmail.com>
2019-06-15 16:16:26 -0500

    Modificaciones a  la conexion

--- proyecto-db/src/database.js
+++ proyecto-db/src/database.js
@@ -2,7 +2,7 @@ const oracle = require('oracledb'),
     {promisify}=require('util'),
     {database} = require('./keys'),
     print = console.log,
-    SQL = `SELECT * FROM product`
+    SQL = `SELECT * FROM user_table`
 
 // const pool = oracle.createPool(database)
 // pool.then(e=>e.getConnection())
@@ -10,10 +10,11 @@ const fnOracle = async ()=>{
     const pool = await oracle.createPool(database)
     let connection = await pool.getConnection()
     let result = await connection.execute(SQL)
+    
     let data = result.rows
     await connection.close()
     return data
 }
 
 module.exports = fnOracle()
-fnOracle().then(data=>console.log(data)).catch(console.error)
+fnOracle().then(data=>print(data)).catch(err=>print(`Error de la base de datos es : ${err.message}`))
