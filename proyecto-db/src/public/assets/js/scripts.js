b7aad177cb4edb8c61b1db6eca3183f31ffd5591
MaurickThom <thomtwd@gmail.com>
2019-06-16 20:40:09 -0500

    Funcion en el frontend para el slider

--- proyecto-db/src/public/assets/js/scripts.js
+++ proyecto-db/src/public/assets/js/scripts.js
@@ -1,1 +1,10 @@
-console.log('hola')
+let images = ['./img/img_1.jpg','./img/img_2.jpg','./img/img_3.jpg']
+
+const slider = container=> {
+    container.addEventListener('click',e=>{
+        let back= container.querySelector('.back'),
+            next = container.querySelector('.next'),
+            img = container.querySelector('img')
+            target = e.target
+    })
+}
