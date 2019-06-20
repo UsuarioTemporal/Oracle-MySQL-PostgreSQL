c9bea08a1c7573c41c6387a0fec7d173fe70f8e1
MaurickThom <thomtwd@gmail.com>
2019-06-19 22:33:51 -0500

    [update slider]

--- proyecto-db/src/public/assets/js/scripts.js
+++ proyecto-db/src/public/assets/js/scripts.js
@@ -4,14 +4,16 @@ const slider = container=> {
     container.addEventListener('click',e=>{
         let back= container.querySelector('.back'),
             next = container.querySelector('.next'),
-            img = container.querySelector('img')
+            img = container.querySelector('img'),
             target = e.target
-        if (!img) return;
-        if (target===back){
+            
+        if (!img) return
+
+        if (target===back)
             index= index===0 ? images.length-1 : index-1
-        }else if(target===next){
+        else if(target===next)
             index= index===images.length-1 ? 0: index+1
-        }
+
         img.src =images[index]
     })
 }
